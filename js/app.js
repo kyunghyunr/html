cf3a4a210e6d22f50a25a7df7a78fa6372082788
kyunghyunr <kyunghyunr@gmail.com>
2022-01-03 00:15:59 +0900

    v2.0.5

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+const openMenu = document.querySelector(".openMenu");
+const mainMenu = document.querySelector(".mainMenu");
 
+openMenu.addEventListener("click", () => {
+  openMenu.classList.toggle("active");
+  mainMenu.classList.toggle("active");
+})
 
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-120%';
-}
+/*
+document.querySelectorAll("").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+*/
